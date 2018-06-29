b5fe9eab45cb26d0e1943c3a1b3a038e74da4712
misupopo <kokorononakaniitumo@yahoo.co.jp>
2018-06-29 17:56:34 +0900

    create resize method

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+class ImageGhostCanvas {
+    constructor(options) {
+        this.size = 320;
+        if (options) {
+            this.size = options.size || this.size;
+        }
+    }
+    resizeImage(imageData, callback) {
+        return new Promise((resolve, reject) => {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            const image = new Image();
+            image.onload = () => {
+                const imgWidth = image.width, imgHeight = image.height, imgRate = imgWidth / imgHeight;
+                let imgPos = 0;
+                canvas.width = this.size;
+                canvas.height = this.size;
+                if (imgRate >= 1.0) {
+                    imgPos = Math.floor((this.size - (this.size * imgRate)) / 2);
+                    ctx.drawImage(image, imgPos, 0, this.size * imgRate, this.size);
+                }
+                else {
+                    imgPos = Math.floor((this.size - (this.size / imgRate)) / 2);
+                    ctx.drawImage(image, 0, imgPos, this.size, this.size / imgRate);
+                }
+                resolve(canvas.toDataURL());
+            };
+            image.onerror = (error) => {
+                reject(new Error('image onerror'));
+            };
+            image.src = imageData;
+        });
+    }
+    ;
+}
+exports.default = ImageGhostCanvas;
---
+++ test/image-ghost-canvas.spec.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+import ImageGhostCanvas from '../index';
+import * as Config from './image-ghost-canvas.config.spec';
+
+describe('ImageGhostCanvas', () => {
+    const urlData = Config.getConfig();
+    const imageGhostCanvas: ImageGhostCanvas = new ImageGhostCanvas({});
+
+    it('should be able to execute resizeImage', async(done) => {
+        await imageGhostCanvas.resizeImage(urlData.imageSrc)
+            .then((file: string) => {
+                expect(urlData.resizedImage).toEqual(file);
+                done();
+            });
+    });
+
+    describe('Error', () => {
+        it('should be error because of text prefix', async(done) => {
+            await imageGhostCanvas.resizeImage('data:text/plain;base64,44OV44Kh44Kk44OI44Gg44KI44Gj77yBCgo=')
+                .catch((error: any) => {
+                    if (error) {
+                        expect(error.message).toEqual('image onerror');
+                        done();
+                    }
+                });
+        });
+
+        it('should be error because of wrong value', async(done) => {
+            await imageGhostCanvas.resizeImage('data:image/png;base64,44OV44Kh44Kk44OI44Gg44KI44Gj77yBCgo=')
+                .catch((error: any) => {
+                    if (error) {
+                        expect(error.message).toEqual('image onerror');
+                        done();
+                    }
+                });
+        });
+    });
+});
